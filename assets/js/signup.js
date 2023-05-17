e7b28282266ae43d5ffff37eace810e7287668f3
Sudheer <121391508+sudheerreddysabbella@users.noreply.github.com>
2023-05-17 15:11:50 +0530

    Update signup.js

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -7,4 +7,12 @@ toggle = () => {
 
 setTimeout(() => {
 	container.classList.add('sign-in')
-}, 200)
+}, 00)
+
+const passwordInput = document.querySelector("#password")
+const eye = document.querySelector("#eye")
+eye.addEventListener("click", function(){
+	this.classList.toggle("fa-eye-slash")
+	const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
+	passwordInput.setAttribute("type", type)
+  })
